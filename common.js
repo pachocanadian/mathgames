87b9b21664102822ce2f90e02e6cceb2cf19d9df
Mike Carr <mcarr@pachogrande.com>
2025-03-07 21:17:24 -0800

    added more success graphics

---
+++ common.js
@@ -0,0 +1,12 @@
+function getRandomInt(x) {
+    // Make sure x is a positive integer
+    if (x <= 1) return 1;
+    
+    // Generate a random integer between 1 and x (inclusive)
+    return Math.floor(Math.random() * x) + 1;
+  }
+
+  function how_many_success_graphics()
+  {
+    return 11;
+  }
