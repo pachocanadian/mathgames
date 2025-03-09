302cf701e173d356e3c586ec37def9df1ffa74f2
Mike Carr <mcarr@pachogrande.com>
2025-03-08 22:23:09 -0800

    updates and improvements

--- common.js
+++ common.js
@@ -1,12 +1,26 @@
 function getRandomInt(x) {
-    // Make sure x is a positive integer
-    if (x <= 1) return 1;
-    
-    // Generate a random integer between 1 and x (inclusive)
-    return Math.floor(Math.random() * x) + 1;
+  // Make sure x is a positive integer
+  if (x <= 1) return 1;
+
+  // Generate a random integer between 1 and x (inclusive)
+  return Math.floor(Math.random() * x) + 1;
+}
+
+function getRandomIntRange(min, max) {
+  // Make sure max is a positive integer
+  if (max <= 1) max = 1;
+
+  // Make sure min is less than or equal to max
+  if (min > max) {
+    var temp = min;
+    min = max;
+    max = temp;
   }
 
-  function how_many_success_graphics()
-  {
-    return 11;
-  }
+  // Generate a random integer between min and max (inclusive)
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function how_many_success_graphics() {
+  return 11;
+}
